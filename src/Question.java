b65adeae647eb6ef4c6f2229ba279c661a483b24
RawalAnuma <rawalanuma@gmail.com>
2025-06-25 23:05:09 +0545

    q

--- src/Question.java
+++ src/Question.java
@@ -17,38 +17,20 @@ public class Question {
     public String getTitle() {
         return title;
     }
-    public void setTitle(String title) {
-        this.title = title;
-    }
     public int getOption1() {
         return option1;
-    }
-    public void setOption1(int option1) {
-        this.option1 = option1;
     }
     public int getOption2() {
         return option2;
     }
-    public void setOption2(int option2) {
-        this.option2 = option2;
-    }
     public int getOption3() {
         return option3;
-    }
-    public void setOption3(int option3) {
-        this.option3 = option3;
     }
     public int getOption4() {
         return option4;
     }
-    public void setOption4(int option4) {
-        this.option4 = option4;
-    }
     public int getCorrectAnswer() {
         return correctAnswer;
-    }
-    public void setCorrectAnswer(int correctAnswer) {
-        this.correctAnswer = correctAnswer;
     }
 
     public boolean checkAnswer(int answer) {
--- src/QuizGame.java
+++ src/QuizGame.java
@@ -2,26 +2,40 @@ import java.util.Scanner;
 
 public class QuizGame {
     public static void main(String[] args) {
-        Question question1 = new Question("What is my age?",20 , 29, 35, 40, 1);
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
 
-        while(true){
-            System.out.println("Question: " + question1.getTitle());
-            System.out.println("1. " + question1.getOption1());
-            System.out.println("2. " + question1.getOption2());
-            System.out.println("3. " + question1.getOption3());
-            System.out.println("4. " + question1.getOption4());
+        Question[] questions = new Question[6];
+        questions[0] = new Question("How many vowels are there in the English alphabet?", 5, 6, 9, 8, 1);
+        questions[1] = new Question("How many alphabets are there in the English language?", 24, 25, 26, 27, 3);
+        questions[2] = new Question("How many continents are there in the world?", 5, 6, 7, 8, 2);
+        questions[3] = new Question("How many planets are there in the solar system?", 7, 8, 9, 10, 2);
+        questions[4] = new Question("What is the height of Mount Everest?", 8848, 8849, 8850, 8851, 1);
+        questions[5] = new Question("What is the value of 4 * 5?", 19, 20, 21, 22, 2);
+
+        int score = 0;
+
+        for (int i = 0; i < questions.length; i++) {
+            Question que = questions[i];
+            System.out.println("Question " + (i + 1) + ": " + que.getTitle());
+            System.out.println("1. " + que.getOption1());
+            System.out.println("2. " + que.getOption2());
+            System.out.println("3. " + que.getOption3());
+            System.out.println("4. " + que.getOption4());
             System.out.print("Please enter your answer (1-4): ");
 
             int userAnswer = scanner.nextInt();
 
-            if (question1.checkAnswer(userAnswer)) {
+            if (que.checkAnswer(userAnswer)) {
                 System.out.println("Correct!");
-                break; // Exit the loop if the answer is correct
+                score++;
             } else {
                 System.out.println("Incorrect, please try again.");
             }
+
+            System.out.println("Final Score: " + score);
         }
     }
 }
